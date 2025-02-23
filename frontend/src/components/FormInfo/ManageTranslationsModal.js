5e1eef6bfbd9271656e31605d60e4a05e56690ac
khalilurrrahmanridoykhan <khalilurrahmanridoykhan@gmail.com>
2025-02-23 14:43:11 +0600

    Language-save-db

--- frontend/src/components/FormInfo/ManageTranslationsModal.js
+++ frontend/src/components/FormInfo/ManageTranslationsModal.js
@@ -6,7 +6,11 @@ const ManageTranslationsModal = ({ show, onHide, formId, onSave }) => {
   const [languages, setLanguages] = useState([]);
   const [defaultLanguage, setDefaultLanguage] = useState('');
   const [defaultLanguageCode, setDefaultLanguageCode] = useState('');
+  const [otherLanguages, setOtherLanguages] = useState([]);
   const [formDetails, setFormDetails] = useState({});
+  const [showOtherLanguagesForm, setShowOtherLanguagesForm] = useState(false);
+  const [showEditDefaultLanguageForm, setShowEditDefaultLanguageForm] = useState(false);
+  const [showEditOtherLanguagesForm, setShowEditOtherLanguagesForm] = useState(false);
 
   useEffect(() => {
     const fetchLanguages = async () => {
@@ -32,6 +36,13 @@ const ManageTranslationsModal = ({ show, onHide, formId, onSave }) => {
           }
         });
         setFormDetails(response.data);
+        setDefaultLanguage(response.data.default_language);
+        setOtherLanguages(response.data.other_languages || []);
+        // Set default language code based on the fetched default language
+        const selectedLanguage = response.data.default_language && languages.find(language => language.id === response.data.default_language);
+        if (selectedLanguage) {
+          setDefaultLanguageCode(selectedLanguage.subtag);
+        }
       } catch (error) {
         console.error('Error fetching form details:', error);
       }
@@ -59,7 +70,12 @@ const ManageTranslationsModal = ({ show, onHide, formId, onSave }) => {
     }
   };
 
-  const handleSave = async () => {
+  const handleOtherLanguagesChange = (e) => {
+    const selectedLanguageIds = Array.from(e.target.selectedOptions, option => option.value);
+    setOtherLanguages(selectedLanguageIds);
+  };
+
+  const handleSaveDefaultLanguage = async () => {
     try {
       const token = sessionStorage.getItem('authToken');
       const payload = {
@@ -73,44 +89,109 @@ const ManageTranslationsModal = ({ show, onHide, formId, onSave }) => {
         }
       });
       onSave();
-      onHide();
+      setShowEditDefaultLanguageForm(false); // Hide the form after saving
     } catch (error) {
-      console.error('Error saving translations:', error);
+      console.error('Error saving default language:', error);
       console.error('Server Response:', error.response.data); // Log the server response
     }
   };
 
+  const handleSaveOtherLanguages = async () => {
+    try {
+      const token = sessionStorage.getItem('authToken');
+      const payload = {
+        ...formDetails,
+        other_languages: otherLanguages
+      };
+      console.log('Request Payload:', payload); // Log the request payload
+      await axios.put(`http://localhost:8000/api/forms/${formId}/`, payload, {
+        headers: {
+          'Authorization': `Token ${token}`
+        }
+      });
+      onSave();
+      setShowEditOtherLanguagesForm(false); // Hide the form after saving
+    } catch (error) {
+      console.error('Error saving other languages:', error);
+      console.error('Server Response:', error.response.data); // Log the server response
+    }
+  };
+
+  const handleClose = () => {
+    onHide();
+    setShowEditDefaultLanguageForm(false); // Reset the form visibility when modal is closed
+    setShowEditOtherLanguagesForm(false); // Reset the form visibility when modal is closed
+  };
+
+  const selectedDefaultLanguage = languages.find(language => language.id === parseInt(defaultLanguage));
+  const selectedOtherLanguages = otherLanguages.map(id => languages.find(language => language.id === parseInt(id))).filter(Boolean);
+
+  // Filter out the default language from the list of available other languages
+  const availableOtherLanguages = languages.filter(language => language.id !== parseInt(defaultLanguage));
+
   return (
-    <Modal show={show} onHide={onHide}>
+    <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Manage Translations</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <p>Please name your default language before adding languages and translations.</p>
-        <Form>
-          <Form.Group controlId="defaultLanguage">
-            <Form.Label>Default Language Name</Form.Label>
-            <Form.Control as="select" value={defaultLanguage} onChange={handleLanguageChange}>
-              <option value="">Select Language</option>
-              {languages.map((language) => (
-                <option key={language.id} value={language.id}>{language.description}</option>
-              ))}
-            </Form.Control>
-          </Form.Group>
-          <Form.Group controlId="defaultLanguageCode">
-            <Form.Label>Default Language Code</Form.Label>
-            <Form.Control as="select" value={defaultLanguageCode} onChange={handleLanguageCodeChange}>
-              <option value="">Select Language Code</option>
-              {languages.map((language) => (
-                <option key={language.id} value={language.subtag}>{language.subtag}</option>
-              ))}
-            </Form.Control>
-          </Form.Group>
-        </Form>
+        {selectedDefaultLanguage && (
+          <p>
+            The default Language: {selectedDefaultLanguage.description} ({selectedDefaultLanguage.subtag})
+          </p>
+        )}
+        {defaultLanguage && !showEditDefaultLanguageForm ? (
+          <Button variant="secondary" onClick={() => setShowEditDefaultLanguageForm(true)}>Edit Default Language</Button>
+        ) : (
+          <Form>
+            <Form.Group controlId="defaultLanguage">
+              <Form.Label>Default Language Name</Form.Label>
+              <Form.Control as="select" value={defaultLanguage} onChange={handleLanguageChange}>
+                <option value="">Select Language</option>
+                {languages.map((language) => (
+                  <option key={language.id} value={language.id}>{language.description}</option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="defaultLanguageCode">
+              <Form.Label>Default Language Code</Form.Label>
+              <Form.Control as="select" value={defaultLanguageCode} onChange={handleLanguageCodeChange}>
+                <option value="">Select Language Code</option>
+                {languages.map((language) => (
+                  <option key={language.id} value={language.subtag}>{language.subtag}</option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Button variant="primary" onClick={handleSaveDefaultLanguage}>Save Default Language</Button>
+          </Form>
+        )}
+        <hr />
+        {selectedOtherLanguages.length > 0 ? (
+          <p>
+            Other Languages: {selectedOtherLanguages.map(lang => `${lang.description} (${lang.subtag})`).join(', ')}
+          </p>
+        ) : (
+          <p>Other Languages:</p>
+        )}
+        {otherLanguages.length > 0 && !showEditOtherLanguagesForm ? (
+          <Button variant="secondary" onClick={() => setShowEditOtherLanguagesForm(true)}>Edit Other Languages</Button>
+        ) : (
+          <Form>
+            <Form.Group controlId="otherLanguages">
+              <Form.Label>Other Languages</Form.Label>
+              <Form.Control as="select" multiple value={otherLanguages} onChange={handleOtherLanguagesChange}>
+                {availableOtherLanguages.map((language) => (
+                  <option key={language.id} value={language.id}>{language.description}</option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Button variant="primary" onClick={handleSaveOtherLanguages}>Save Other Languages</Button>
+          </Form>
+        )}
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={onHide}>Close</Button>
-        <Button variant="primary" onClick={handleSave}>Save</Button>
+        <Button variant="secondary" onClick={handleClose}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
